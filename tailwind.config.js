d1920b33a3790503ebf65f0d690d5d91bbcc5848
GathaiKE <gathaikariuki@gmail.com>
2023-09-23 16:43:08 +0300

    Updated the landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,8 @@ module.exports = {
         primary:{
           'light':"rgb(26,34,71)",
           'dark':"#0A0F25",
-          'darker':"#060916"
+          'darker':"#060916",
+          'hero':"rgba(0,0,0,0.7)"
         },
         cyan:"rgb(0,255,255)",
         white:"rgb(255,255,255)",
