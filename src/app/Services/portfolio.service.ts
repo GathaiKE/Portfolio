0c71a4e65ae80c9ee980c6d5da9aeadaba52ab4b
Gathai <briannjeri9@gmail.com>
2024-01-02 19:27:38 +0300

    Removed lorem Ipsum text from the site.

--- src/app/Services/portfolio.service.ts
+++ src/app/Services/portfolio.service.ts
@@ -24,16 +24,19 @@ export class PortfolioService {
     salutation:string="Hello and welcome to my world, My name is"
     career:string="Full-Stack Software Delveloper"
     pitchText:string=`
-    I'm an experienced Full Stack Software Developer with a deep passion for crafting high-quality digital solutions for my clients. In my world, I explore the limitless possibilities that code can unlock. Join me on a journey through my work and discover the power of innovation.
+    Welcome to the realm where lines of code craft dreams into reality! I'm a seasoned Full Stack Software Developer fueled by a relentless passion for weaving digital marvels. Journey alongside me as we unravel the boundless possibilities that code unveils, exploring the transformative power of innovation woven into every pixel and line of my work..
     `
+    // pithText:string=`
+    // I'm an experienced Full Stack Software Developer with a deep passion for crafting high-quality digital solutions for my clients. In my world, I explore the limitless possibilities that code can unlock. Join me on a journey through my work and discover the power of innovation
+    // `
 
     //About Page
     aboutStatement:string=`
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo nihil saepe corporis perferendis explicabo reiciendis adipisci expedita totam ullam! Nisi, neque esse inventore quas doloribus velit impedit nesciunt quis voluptate alias! Velit voluptate molestiae minus itaque. Ab maiores ducimus fuga molestias sed quia aspernatur nam quaerat inventore amet saepe repudiandae a quisquam architecto recusandae maxime, earum eligendi blanditiis suscipit. Quod optio aliquid inventore accusamus quisquam ipsam rerum aliquam nostrum doloremque natus, maiores tenetur deleniti in maxime voluptate, perferendis quaerat! Facilis assumenda optio blanditiis ex. Perspiciatis aliquam similique iure praesentium officiis illum magnam eos, expedita odit deserunt ducimus, autem nisi reiciendis.
+    So who is Gathai? I am a passionate full-stack developer passionate about crafting innovative solutions that bridge the gap between intricate backend systems and intuitive user interfaces. With a solid foundation in a variety of programming languages, frameworks, and tools, I thrive on the challenges inherent in the software development realm. Throughout my career, I've had the privilege of leveraging my skills to build projects that optimize database performance, design seamless user experiences, and develop robust backend infrastructures. My approach combines technical prowess with a collaborative spirit, enabling me to seamlessly integrate within multidisciplinary teams and deliver high-quality products. I'm deeply committed to staying updated on the latest industry trends and technologies, ensuring that my work remains at the forefront of innovation. I'm always excited about the opportunity to contribute my skills to forward-thinking projects that push boundaries and make a meaningful impact so if you have one, I am waiting to hear about it.
     `
 
-    myLSkills:string[]=['HTML',' CSS','JavaScript','Typescript','Angular','Node']
-    myRSkills:string[]=['Express','Redux','MySQL','TailwindCSS','FireBase','AWS']
+    myLSkills:string[]=['HTML',' CSS','JavaScript','Typescript','Angular','Node','PHP']
+    myRSkills:string[]=['Express','Redux','MySQL','TailwindCSS','FireBase','AWS','MongoDB']
     profilePicture:string='../../assets/IMG-20221220-WA0010.jpg'
 
     //Experience Page
@@ -46,7 +49,7 @@ export class PortfolioService {
         startDate:"April 2023",
         endDate:"June 2023",
         JobDescription:`
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia totam quis molestiae, magnam velit nulla sunt distinctio, illo hic dignissimos voluptates cum iste iure quos id est deserunt tempore modi ullam fugiat, similique ipsum! Itaque doloremque, dolore tenetur placeat repellat possimus minima earum perspiciatis labore! Placeat saepe mollitia culpa quos aut magnam temporibus ratione natus harum iste modi, ex iusto distinctio vero molestias ea laboriosam numquam sint molestiae consequuntur quaerat soluta cumque. Dicta voluptatem, ex rerum fugiat non sequi ad sint illo maiores magni minus dignissimos sunt perspiciatis? Quis facere eaque dolore eveniet sunt molestias eius deleniti. Atque officiis consequatur est similique natus mollitia quo sunt libero, excepturi, accusamus ad quis quisquam minus soluta odio. Rem, nam quis libero quidem laudantium ipsa officia accusantium consectetur, provident est in deleniti saepe reiciendis tempora repellendus sit asperiores tenetur et, id sed commodi similique! Totam in at veritatis rem quisquam impedit maxime quis, doloribus vitae voluptates dignissimos, consequatur enim ex blanditiis. Expedita eius voluptatibus repellendus voluptate, omnis quisquam vel voluptas rerum tenetur numquam ipsa, inventore vitae error cum. Minus, illo repellendus. Repellendus vel aliquid fuga, laborum illo eveniet voluptate incidunt commodi vero aspernatur blanditiis officia esse molestias facilis! Enim soluta harum unde autem.
+        "During my tenure as a Software Developer Intern at The Jitu, I actively contributed to the development team, gaining hands-on experience in software design, coding, testing, and debugging. Working closely with seasoned developers, I participated in various stages of the software development lifecycle, assisting in the creation and enhancement of applications. This internship provided me with valuable insights into agile methodologies, collaborative problem-solving, and the practical application of programming languages and frameworks in a professional environment. I engaged in tasks such as web design, front-end and backend development, designing and developing databases, state management, unit and end to end testing among others, which allowed me to apply theoretical knowledge to real-world scenarios while refining my technical skills and fostering a deeper understanding of software development practices.
         `
       }
     ]
@@ -58,11 +61,11 @@ export class PortfolioService {
         id:1,
         title:"Portfolio",
         displayImage:"../../assets/portfolio.png",
-        liveLink:"",
-        githubLink:"",
+        liveLink:"https://gathaikariuki.vercel.app/",
+        githubLink:"https://github.com/GathaiKE/Portfolio",
         technologiesUsed:['Angular','Node','Express','MsSQL','TailwindCSS','Typescript','NgRx'],
         description:`
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, impedit, eveniet itaque reiciendis aspernatur accusamus quibusdam officiis laborum at maxime pariatur amet corporis? Consequatur alias pariatur accusamus iste repudiandae et veniam! Provident, suscipit. Consequuntur harum reprehenderit assumenda ducimus saepe facilis, maiores doloremque ratione velit repellendus voluptas illum. Explicabo alias modi quod numquam cupiditate, similique consectetur, nam minus esse at nihil ut vitae reprehenderit voluptatem sunt beatae illum ullam maxime magni! Repudiandae cupiditate at fugit molestias, saepe ipsam! Dicta commodi dolorum accusamus quae iste quo amet, aliquid modi quos hic ducimus laudantium, nemo alias dolores aliquam! Eaque veritatis id vero facere!
+        My portfolio serves as my inaugural and primary project, embodying my journey and showcasing my skills as a burgeoning developer. Crafted with meticulous attention to detail, this project represents a culmination of my technical expertise, creativity, and passion for software development. Designed as a showcase of my capabilities, it encapsulates a blend of intuitive user interface design and robust backend functionalities. Leveraging a modern tech stack and employing responsive design principles, my portfolio demonstrates my proficiency in designing beautiful interfaces with figma and interpreting the design into realistic and responsive websites using Angular and Tailwind CSS frameworks. Each element, from layout to interactive features, has been thoughtfully curated to not only exhibit my technical prowess but also reflect my personal style and commitment to excellence. Through this project, I aim to present my skills, experiences, and aspirations while providing visitors with an immersive glimpse into my capabilities and achievements in the world of software development."
         `,
         production:false
       }
@@ -85,17 +88,17 @@ export class PortfolioService {
     {
       id:2,
       image:'',
-      name:"John Mwaniki",
+      name:"Jonathan Ndambuki",
       text:`
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque eos aliquid eius asperiores laborum quaerat provident repellendus? Nemo, totam! Dolorem magnam vitae suscipit accusamus, eius eveniet quaerat sint commodi maxime adipisci tempora ad Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque eos aliquid eius asperiores laborum quaerat provident repellendus? Nemo, totam! Dolorem magnam vitae suscipit accusamus, eius eveniet quaerat sint commodi maxime adipisci tempora ad
+      Working with him, I have seen a strong passion for creating scripts and problem-solving, displaying an eager willingness to learn. He demonstrated a proactive approach to acquiring new knowledge and was quick to grasp instructions. His adaptability shone through whenever adjustments to the initial plan were required. Notably, he effectively communicated ideas both verbally and in written form, showcasing adeptness in interpersonal skills across different mediums.
       `
     },
     {
       id:3,
       image:'',
-      name:"Brian Gathai",
+      name:"John Mwaniki",
       text:`
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque eos aliquid eius asperiores laborum quaerat provident repellendus? Nemo, totam! Dolorem magnam vitae suscipit accusamus, eius eveniet quaerat sint commodi maxime adipisci tempora ad Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque eos aliquid eius asperiores laborum quaerat provident repellendus? Nemo, totam! Dolorem magnam vitae suscipit accusamus, eius eveniet quaerat sint commodi maxime adipisci tempora ad
+      He exhibited genuine enthusiasm for script development and problem-solving, driven by an inherent curiosity to explore new concepts. His rapid comprehension of instructions, coupled with a flexible attitude towards adapting to evolving project requirements, underscored his adeptness. His communication skills, proficient in both verbal and written forms, facilitated effective collaboration and clear articulation of ideas, contributing significantly to the team's productivity and success
       `
     }
   ]
